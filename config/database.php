67d61dc2e5779aebf58b280b59535ad6ad8db19b
Benjamin <wangbin042@gmail.com>
2019-05-16 11:36:27 +0000

    change database config3

--- config/database.php
+++ config/database.php
@@ -17,7 +17,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => getenv('IS_IN_HEROKU') ? 'pgsql' : env('DB_CONNECTION', 'mysql'),
 
     /*
     |--------------------------------------------------------------------------

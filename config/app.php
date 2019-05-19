c084ed476cfa20aa6712d38c0ade753dd9660769
Benjamin <wangbin042@gmail.com>
2019-05-19 13:27:16 +0000

    change appurl

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://http://fzubbs.herokuapp.com'),
+    'url' => env('APP_URL', 'http://fzubbs.herokuapp.com'),
 
     'asset_url' => env('ASSET_URL', null),
 
